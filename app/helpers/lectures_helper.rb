ee9eba71d367bcb0cc97cd849c331d7dbb390935
Sulman Baig <sulman1990@hotmail.com>
2016-01-14 11:37:23 +0500

    changed lecture helper a bit

--- app/helpers/lectures_helper.rb
+++ app/helpers/lectures_helper.rb
@@ -1,7 +1,9 @@
 module LecturesHelper
   def linked_users(content)
-    content.gsub /@([\w]+.[\w]+)/ do |match|
-      link_to match, user_path(match.to_s[1..-1])
-    end.html_safe
+    if content.present?
+      content.gsub /@([\w]+.[\w]+)/ do |match|
+        link_to match, user_path(match.to_s[1..-1])
+      end.html_safe
+    end
   end
 end
