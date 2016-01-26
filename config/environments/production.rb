c0e1ecd9a36c5ab04ab9e549c8839c1d1fdb1f1c
Sulman Baig <sulman1990@hotmail.com>
2016-01-26 19:11:58 +0500

    changing pepipost to mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,11 +81,11 @@ Myapp::Application.configure do
   config.action_mailer.default_url_options = {host: 'codelit.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      address: 'smtp.pepipost.com',
+      address: 'smtp.mandrillapp.com',
       port: 587,
       enable_starttls_auto: true,
-      user_name: ENV['PEPIPOST_USERNAME'],
-      password: ENV['PEPIPOST_PASSWORD'],
+      user_name: ENV['MANDRILL_USERNAME'],
+      password: ENV['MANDRILL_PASSWORD'],
       authentication: 'login'
   }
 end
